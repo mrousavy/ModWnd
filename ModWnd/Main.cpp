09b702b53453ebfea7cd3f59cb166c556765424d
Marc Rousavy <marcrousavy@hotmail.com>
2017-12-04 15:32:42 +0100

    Add main func

---
+++ ModWnd/Main.cpp
@@ -0,0 +1,13 @@
+#include "CaptainHook.h"
+#include "MoveManager.h"
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+	std::cout << "Initializing ModWnd.." << std::endl;
+	
+
+	getchar();
+	return 0;
+}
