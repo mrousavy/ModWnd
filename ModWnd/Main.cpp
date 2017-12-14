d0f25f3d7394de35ea06ff7b2fa175095cdc3390
Marc Rousavy <marcrousavy@hotmail.com>
2017-12-14 08:37:43 +0100

    Handle Hotkey argument

--- ModWnd/Main.cpp
+++ ModWnd/Main.cpp
@@ -4,15 +4,23 @@
 #include <Windows.h>
 
 
-int main(int argc, char** argv)
+int main(const int argc, char** argv)
 {
+	int hotkey = VK_MENU;
+	if(argc > 0)
+	{
+		hotkey = atoi(argv[0]);
+		if (hotkey > MAXINT || hotkey < 1)
+			hotkey = VK_MENU;
+	}
+
 	// Print Init Message
 	std::cout << "Initializing ModWnd.." << std::endl;
 
 	// Hide the Console Window
 	//ShowWindow(GetConsoleWindow(), SW_HIDE);
 	
-	captain_hook hook(VK_MENU);
+	captain_hook hook(hotkey);
 	hook.start();
 
 	getchar();
