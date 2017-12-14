d9b226cee040164358b13c03b0bb5a8f7901f375
Marc Rousavy <marcrousavy@hotmail.com>
2017-12-14 12:00:58 +0100

    Rewrite moving

--- ModWnd/MoveManager.h
+++ ModWnd/MoveManager.h
@@ -10,11 +10,9 @@ inline void move_wnd(const HWND window, const int x, const int y)
 	RECT rect;
 	GetWindowRect(window, &rect);
 
-	const int dx = x - rect.left;
-	const int dy = y - rect.top;
-	const int to_x = x + dx;
-	const int to_y = y + dy;
+	const int to_x = x + x - rect.left;
+	const int to_y = y + y - rect.top;
 
-	SetWindowPos(window, nullptr, to_x, to_y, 0, 0, SWP_NOOWNERZORDER | SWP_NOREDRAW | SWP_NOSIZE);
-	//MoveWindow(window, to_x, to_y, width, height, true);
+	//SetWindowPos(window, nullptr, to_x, to_y, 0, 0, SWP_NOREDRAW | SWP_NOSIZE);
+	MoveWindow(window, to_x, to_y, rect.right - rect.left, rect.bottom - rect.top, true);
 }
